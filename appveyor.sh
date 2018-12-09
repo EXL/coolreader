a2c0ea72698b6c1e1d795129cb7b9ccccc0c1fd2
EXL <exlmotodev@gmail.com>
2018-12-09 13:55:30 +0700

    AppVeyor CI: Upload Qt translation file

--- appveyor.sh
+++ appveyor.sh
@@ -17,10 +17,13 @@ cd ../
 mkdir build/
 cd build/
 
-cmake -G"MSYS Makefiles" -DCMAKE_PREFIX_PATH=$MINGW_PREFIX/qt5-static/ -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS:STRING="-static -static-libgcc -static-libstdc++" -DQT_QMAKE_EXECUTABLE=$MINGW_PREFIX/qt5-static/bin/qmake.exe -DUSE_QT_PNG=1 -DCMAKE_CXX_FLAGS:STRING="-Os -O3 -D_USE_ALL_SYSTEM_FONTS" -DGUI=QT5 -DMAX_IMAGE_SCALE_MUL=2 -DDOC_DATA_COMPRESSION_LEVEL=3 -DDOC_BUFFER_SIZE=0x1400000 -DBIG_PAGE_MARGINS=1 ../coolreader
+cmake -G"MSYS Makefiles" -DCMAKE_PREFIX_PATH=$MINGW_PREFIX/qt5-static/ -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS:STRING="-static -static-libgcc -static-libstdc++" -DQT_QMAKE_EXECUTABLE=$MINGW_PREFIX/qt5-static/bin/qmake.exe -DUSE_QT_PNG=1 -DCMAKE_CXX_FLAGS:STRING="-Os -O3 -D_USE_ALL_SYSTEM_FONTS" -DGUI=QT5 -DMAX_IMAGE_SCALE_MUL=2 -DDOC_DATA_COMPRESSION_LEVEL=3 -DDOC_BUFFER_SIZE=0x1400000 -DBIG_PAGE_MARGINS=1 -DCMAKE_INSTALL_PREFIX=../install_cr3 ../coolreader
 
 make V=1 -j4
+make install
 
-strip -s cr3qt/cr3.exe
-mv cr3qt/cr3.exe ./cr3-qt5.exe
+cd ../install_cr3/
+strip -s cr3.exe
+mv cr3.exe cr3-qt5.exe
 curl -sS --upload-file cr3-qt5.exe https://transfer.sh/cr3-qt5.exe && echo -e '\n'
+curl -sS --upload-file i18n/qtbase_ru.qm https://transfer.sh/qtbase_ru.qm && echo -e '\n'
